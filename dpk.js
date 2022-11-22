f9b49a12379fdc348df3c59a8559acd3ab4e85bb
Kenneth Lee <kenneth.lee.developer@gmail.com>
2022-11-22 12:15:20 -0800

    Made helper function

--- dpk.js
+++ dpk.js
@@ -3,6 +3,14 @@ const crypto = require("crypto");
 const TRIVIAL_PARTITION_KEY = "0";
 const MAX_PARTITION_KEY_LENGTH = 256;
 
+const turnToString = (cand) => {
+  if (typeof cand !== 'string') {
+    return JSON.stringify(cand);
+  }
+
+  return cand
+}
+
 exports.deterministicPartitionKey = (event) => {
   let candidate;
 
@@ -13,14 +21,12 @@ exports.deterministicPartitionKey = (event) => {
     candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-
   if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+    candidate = turnToString(candidate);
   } else {
     candidate = TRIVIAL_PARTITION_KEY;
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
